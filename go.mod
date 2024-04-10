3078157ce11e0cd9dea0c138dc7b02ff90e1a305
idan <idankob@gmail.com>
2024-04-10 20:04:18 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module tera-scanner
+
+go 1.22
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		executable, err := os.Executable()
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		log.Fatalf(":: Usage: go run %s.go <path>", filepath.Base(executable))
+		return
+	}
+
+	minecraftDir := os.Args[1]
+
+	servers, err := scanMinecraftServers(minecraftDir)
+	if err != nil {
+		log.Fatalf("Error scanning Minecraft servers: %v", err)
+		return
+	}
+
+	fmt.Println("Scanning servers:")
+	for server, worlds := range servers {
+		fmt.Printf("Server %s, Worlds: %d\n", server, len(worlds))
+	}
+
+	start := time.Now()
+	fmt.Printf("Run time: %d\v", time.Since(start))
+}
+
+func scanMinecraftServers(dir string) (map[string][]string, error) {
+	servers := make(map[string][]string)
+
+	var wg sync.WaitGroup
+
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatalf("Error acure while readine the dir: %s :: %v\n", dir, err)
+		return nil, err
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			wg.Add(1)
+			go func(serverName string) {
+				defer wg.Done()
+				if isMinecraftServer(filepath.Join(dir, serverName)) {
+					worlds, err := countWorlds(filepath.Join(dir, serverName))
+					if err != nil {
+						log.Fatalf("Error counting worlds for server %s :: %v", serverName, err)
+						return
+					}
+
+					servers[serverName] = worlds
+				}
+			}(file.Name())
+		}
+	}
+
+	wg.Wait()
+
+	return servers, nil
+}
+
+func isMinecraftServer(dir string) bool {
+	_, err := os.Stat(filepath.Join(dir, "server.properties"))
+	return err == nil
+}
+
+func countWorlds(dir string) ([]string, error) {
+	var worlds []string
+
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatalf("Error acure while readine the dir: %s :: %v\n", dir, err)
+		return nil, err
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			_, err := os.Stat(filepath.Join(dir, file.Name(), "uid.dat"))
+			if err == nil {
+				worlds = append(worlds, file.Name())
+			}
+		}
+	}
+
+	return worlds, nil
+}
